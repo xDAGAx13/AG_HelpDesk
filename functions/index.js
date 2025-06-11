4b27a2031be8f486748ab0dbf5c8f78736a4d3cb
xDAGAx13 <rohandaga05@gmail.com>
2025-06-11 13:58:12 +0530

    updated forgot pwd code

--- functions/index.js
+++ functions/index.js
@@ -26,7 +26,7 @@ const createTransporter = (email, password) =>
 const departmentEmails = {
   IT: ["nandita@agpolypacks.com"],
   HR: ["hr@agpolypacks.com"],
-  Accounts: ["ayushi.shah@agpolypacks.com"],
+  Accounts: ["aayushi.shah@agpolypacks.com"],
   Admin: ["nandita@agpolypacks.com"],
   Hiring: ["hiring@agpolypacks.com"],
 };
